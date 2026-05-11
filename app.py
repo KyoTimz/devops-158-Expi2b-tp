072d527db0c0ec0637ca06904640799aa7ac05a5
KyoTimz <claudioortiga@gmail.com>
2026-05-11 11:41:18 +0200

    Test demo

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return "Hello Oskouuuuuuuuuur Batman tjukgfjufgwuigfuiest  weeee"
+    return "Oskouuuuuuuuuur Batman tjukgfjufgwuigfuiest  weeee"
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
