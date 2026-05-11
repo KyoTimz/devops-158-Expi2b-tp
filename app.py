9005f8f96e3415bb5d29fd8be4b4f1206db8c70e
KyoTimz <claudioortiga@gmail.com>
2026-05-11 11:22:53 +0200

    Modification du message Flask

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return "Oskouuuuuuuuuur Batman test"
+    return "Oskouuuuuuuuuur Batman test  weeee"
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
